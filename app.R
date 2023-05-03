b25cdad0cd5164fe58cf76538d7368fd7cc02d48
Karl Myers <126172945+RagnaroniKarl@users.noreply.github.com>
2023-05-03 15:29:56 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -5,57 +5,74 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-                tags$style(".container-fluid {
+  tags$style(".container-fluid {
                              background-color: #47ADF0;
               }"),
-
-
-    # Application title
-    titlePanel(
-      h1("Welcome to CatChooser.com!", style = "color:red"),
+  
+  
+  # Application title
+  titlePanel(
+    h1("Welcome to CatChooser.com!", style = "color:red"),
+  ),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      h3("About us : "),
+      p("This is a web application", 
+        strong(" for our Data Science project,",),
+        " so we decided on this! CatChooser! We think it will be very useful for many people who are interested by cats."),
+      br(),
+      p("We are three people who are working on this project. So we'll see how everything goes!"),
+      
+      #dateInput("date",
+                #h3("Input your Date here", style = "color:orange"),
+                #value = "2023-04-23")
     ),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          h3("About us : "),
-          p("This is a web application", 
-            strong(" for our Data Science project,",),
-            " so we decided on this! CatChooser! We think it will be very useful for many people who are interested by cats."),
-          br(),
-          p("We are three people who are working on this project. So we'll see how everything goes!"),
-          
-          dateInput("date",
-           h3("Input your Date here", style = "color:orange"),
-           value = "2023-04-23")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          h1("This will be the main page of the site"),
-          p("Please choose the feature/function you need to use currently!", style = "font-family: 'times"),
-          tabsetPanel(type = "tab", 
-                      tabPanel("Introduction", textOutput("introduction")),
-                      tabPanel("Different Cat Breeds", textOutput("breeds")),
-                      tabPanel("Vetenarians Near Me", textOutput("vets")),
-                      tabPanel("Adoption Centers Near Me", textOutput("adoption")),
-                      tabPanel("About Us", textOutput("aboutus"))),
-          selectInput("select", h3("Select your cat breed :"),
-                      choices = list("Select your breed..." = 1, "Abyssinian" = 2, "American Bobtail" = 3, "American Shorthair" = 4, "Balinese" = 5, "Bengal" = 6, "Birman" = 7, "Bombay" = 8, "British Shorthair" = 9, "Devon Rex" = 10, "Domestic Longhair" = 11, "Exotic Shorthair" = 12, "Himalayan" = 13, "Maine Coon" = 14, "Norwegian Forest" = 15, "Persian" = 16, "Ragdoll" = 17, "Savannah" = 18, "Scottish Fold" = 19, "Siamese" = 20, "Sphynx" = 21), selected = 1)
-                      )
-        )
-        )
     
+    # Show a plot of the generated distribution
+    mainPanel(
+      h1("This will be the main page of the site"),
+      p("Please choose the feature/function you need to use currently!", style = "font-family: 'times"),
+      tabsetPanel(type = "tab", 
+                  tabPanel("Introduction", strong("This is the introduction page. Thank you for using our website!"), textOutput("introduction")),
+                  tabPanel("Different Cat Breeds",p("This is the section that talks about the various cat breeds, their personalities, prices, lifespan and much more.
+    We will show an image of the selected cat breed, display important information about that particular cat breed including : "), 
+     br(),                      
+    p("- Description of the Breed"),
+    br(),
+    p("- Average Lifespan"),
+    br(),
+    p("- Average Weight and Size"),
+    br(),
+    p("- Average Price"),
+    br(),
+    p("- What that Cat Breed is good with (Dogs, Kids, etc)."),  textOutput("breeds")),
+                  tabPanel("Vetenarians Near Me", textOutput("vets")),
+                  tabPanel("Adoption Centers Near Me", textOutput("adoption")),
+                  tabPanel("About Us", textOutput("aboutus"))),
+      selectInput("select", h3("Select your cat breed :"),
+                  choices = list("Select your breed..." = 1, "Abyssinian" = 2, "American Bobtail" = 3, "American Shorthair" = 4, "Balinese" = 5, "Bengal" = 6, "Birman" = 7, "Bombay" = 8, "British Shorthair" = 9, "Devon Rex" = 10, "Domestic Longhair" = 11, "Exotic Shorthair" = 12, "Himalayan" = 13, "Maine Coon" = 14, "Norwegian Forest" = 15, "Persian" = 16, "Ragdoll" = 17, "Savannah" = 18, "Scottish Fold" = 19, "Siamese" = 20, "Sphynx" = 21), selected = 1)
+    )
+  )
+)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-  introduction <- ({
-  "This is the introduction page. Thank you for using our website!"
-  })
+  
+  #introduction <- ({
+    #"This is the introduction page. Thank you for using our website!"
+ # })
   
   breeds <- ({
-    "This is the section that talks about the various cat breeds, their personalities, prices, lifespan and much more."
+    p("This is the section that talks about the various cat breeds, their personalities, prices, lifespan and much more.
+    We will show an image of the selected cat breed, display important information about that particular cat breed including :
+    - Description of the Breed
+    - Average Lifespan
+    - Average Weight and Size
+    - Average Price
+    - What that Cat Breed is good with (Dogs, Kids, etc).")
   })
   
   vets <- ({
@@ -93,3 +110,4 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
