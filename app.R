291597e8ab56cce2108f5cd278bac84e362890b7
hugo-steph28 <126177471+hugo-steph28@users.noreply.github.com>
2023-05-17 15:43:19 +0200

    update

--- app.R
+++ app.R
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-
-
-#package necessary to intall in order to run our app 
-
-#install.packages("shiny")
-#install.packages("leaflet")
-#install.packages("shinydashboard")
-#install.packages("shinythemes")
-
-
-
-
-=======
->>>>>>> Stashed changes
 #this is the cat chooser shiny page
 
 library(shiny)
@@ -21,20 +6,12 @@ library(shinydashboard)
 library(shinythemes)
 
 # Define UI for application that draws a histogram
-<<<<<<< Updated upstream
-ui <- fluidPage(theme = shinytheme("cyborg"),
-=======
 ui <- fluidPage(theme = shinytheme("sandstone"),
->>>>>>> Stashed changes
                 
                 
                 # Application title
                 titlePanel(
-<<<<<<< Updated upstream
-                  h1("Welcome to CatChooser.com!", style = "color:cyan", align = 'center'),
-=======
                   h1("Welcome to CatChooser.com!", style = "color:#FFD700", align = 'center'),
->>>>>>> Stashed changes
                 ),
                 
                 #Sidebar panel
@@ -47,27 +24,8 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                                 
                                 
                                 
-<<<<<<< Updated upstream
-                                #Introduction Tab  
-                                
-                                tabPanel("Introduction", p("Welcome to our web application, your ultimate destination for everything cat-related."),
-                                    br(),
-                                    p("Whether you're a proud cat owner, a cat enthusiast, or someone looking to learn more about these fascinating creatures, you've come to the right place."),
-                                    br(),
-                                    p("Our application is designed to provide you with a comprehensive experience when it comes to cat knowledge, veterinary services, and information about different cat breeds. Whether you're seeking advice on daily care, health, nutrition, or cat behavior, our application is filled with reliable resources and informative content written by experts."),
-                                    br(), 
-                                    p("One of the key features of our application is the ability to find cats available for adoption. We work closely with shelters and animal protection associations to help you find the perfect feline companion for your home. You can browse through our database of adorable cats, filter by breed, age, and location, and get in touch with the shelters to initiate the adoption process."),
-                                    br(),
-                                    p("We understand that your cat's health is a top priority. That's why our application also allows you to find veterinarians specialized in feline care near your location. You can explore veterinarian profiles, read customer reviews, and book appointments online, all with just a few clicks."),
-                                    br(),
-                                    p("Additionally, our application offers a dedicated section on different cat breeds. Whether you're curious to discover the unique characteristics of certain breeds or want to learn more about the history and evolution of cats, our informative content will provide you with the answers you seek.
-Join our community of cat lovers today and dive into the wonderful world of these adorable felines. Download our application and access a wealth of information, trusted veterinary resources, and an opportunity to adopt a feline companion that will brighten your life.
-
-Welcome to the world of cats!")),
-=======
                                 #Introduction Tab                
                                 tabPanel("Introduction", strong("This is the introduction page. Thank you for using our website!")),
->>>>>>> Stashed changes
                                 
                                 
                                 
