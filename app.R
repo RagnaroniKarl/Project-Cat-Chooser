2a4f21dfe2a88d890dc1323fa9f4912bef569c2f
Karl Myers <126172945+RagnaroniKarl@users.noreply.github.com>
2023-05-17 15:44:36 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -1,3 +1,15 @@
+
+
+#package necessary to intall in order to run our app 
+
+#install.packages("shiny")
+#install.packages("leaflet")
+#install.packages("shinydashboard")
+#install.packages("shinythemes")
+
+
+
+
 #this is the cat chooser shiny page
 
 library(shiny)
@@ -11,7 +23,7 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                 
                 # Application title
                 titlePanel(
-                  h1("Welcome to CatChooser.com!", style = "color:#FFD700", align = 'center'),
+                  h1("Welcome to CatChooser.com!", style = "color:cyan", align = 'center'),
                 ),
                 
                 #Sidebar panel
@@ -24,8 +36,23 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                                 
                                 
                                 
-                                #Introduction Tab                
-                                tabPanel("Introduction", strong("This is the introduction page. Thank you for using our website!")),
+                                #Introduction Tab  
+                                
+                                tabPanel("Introduction", p("Welcome to our web application, your ultimate destination for everything cat-related."),
+                                    br(),
+                                    p("Whether you're a proud cat owner, a cat enthusiast, or someone looking to learn more about these fascinating creatures, you've come to the right place."),
+                                    br(),
+                                    p("Our application is designed to provide you with a comprehensive experience when it comes to cat knowledge, veterinary services, and information about different cat breeds. Whether you're seeking advice on daily care, health, nutrition, or cat behavior, our application is filled with reliable resources and informative content written by experts."),
+                                    br(), 
+                                    p("One of the key features of our application is the ability to find cats available for adoption. We work closely with shelters and animal protection associations to help you find the perfect feline companion for your home. You can browse through our database of adorable cats, filter by breed, age, and location, and get in touch with the shelters to initiate the adoption process.", align = 'left', img(src = "kitten-adoption.jpg", height = 200, width = 200, align = 'right')),
+                                    
+                                    br(),
+                                    p("We understand that your cat's health is a top priority. That's why our application also allows you to find veterinarians specialized in feline care near your location. You can explore veterinarian profiles, read customer reviews, and book appointments online, all with just a few clicks."),
+                                    br(),
+                                    p("Additionally, our application offers a dedicated section on different cat breeds. Whether you're curious to discover the unique characteristics of certain breeds or want to learn more about the history and evolution of cats, our informative content will provide you with the answers you seek.
+Join our community of cat lovers today and dive into the wonderful world of these adorable felines. Download our application and access a wealth of information, trusted veterinary resources, and an opportunity to adopt a feline companion that will brighten your life.
+
+Welcome to the world of cats!")),
                                 
                                 
                                 
